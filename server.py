8aba553434eb16ebe09fd6fd8e8a990753c5991d
sairpa <pranav.ajay2000@gmail.com>
2021-05-05 14:36:24 +0530

    Final commit! 😎
    Signed-off-by: sairpa <pranav.ajay2000@gmail.com>

--- server.py
+++ server.py
@@ -111,18 +111,18 @@ def root():
     wiki_soup = BeautifulSoup(wiki_page, 'html.parser')
     wiki_soup.prettify()
     title = wiki_soup.find(id="firstHeading")
-    print(title.string)
+    # print(title.string)
     wiki_content=wiki_soup.find_all(id="mw-content-text")
     wiki_desc_raw = wiki_soup.find_all('p')
     wiki_desc = [comment.text for comment in wiki_desc_raw]
-    print(wiki_desc[0])
+    # print(wiki_desc[0])
     gfg_link = "https://www.geeksforgeeks.org/quad-tree/"
     gfg_page = urllib.request.urlopen(gfg_link)
     gfg_soup = BeautifulSoup(gfg_page, 'lxml')
     gfg_div_text = gfg_soup.find_all("div", attrs={'class': 'text'})
     gfg_desc_raw = gfg_soup.find_all('p')
     gfg_desc = [comment.text for comment in gfg_desc_raw]
-    print(gfg_desc[0])  # gfg description
+    # print(gfg_desc[0])  # gfg description
     k = gfg_desc[0]
     gfg_steps_raw = gfg_soup.find_all('ol')
     gfg_steps = [comment.text for comment in gfg_steps_raw]
